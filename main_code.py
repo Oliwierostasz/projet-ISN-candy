bd28884a2e983f8a08587102d9bc81360def3d7b
Shaunipotte <128689352+Shaunipotte@users.noreply.github.com>
2023-03-30 10:14:51 +0200

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -133,3 +133,13 @@ def regeneration(grille,score):
         grille = gravite(grille)
     return grille, score
    
+    
+ def affichage_grille(grille, nb_type_bonbons):
+ """
+Affiche la grille de jeu "grille" contenant au  maximum "nb_type_bonbons" couleurs de bonbons différentes.
+ """
+    plt.imshow(grille, vmin=0, vmax=nb_type_bonbons−1, cmap=’jet’)
+    plt.pause(0.1)
+    plt.draw()
+    plt.pause(0.1)
+
