cb31057b393322764270fefc93b506a81462d688
Shaunipotte <128689352+Shaunipotte@users.noreply.github.com>
2023-04-06 11:26:14 +0200

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -126,38 +126,12 @@ def recherche_combinaison_grille(grille):
     ttes_combis = []
     for i in range (len(grille)):
         for j in range(len(grille)):
-            ttes_combis += (detecte_coordonnees_combinaison(grille,i,j))
-    parcours = 0
-    while parcours <= len(ttes_combis):
-        for n in range(parcours+1,len(ttes_combis)):
-            if ttes_combis[parcours] == ttes_combis[n]:
-                del ttes_combis[n]
-        parcours += 1
+            if detecte_coordonnees_combinaison(grille,i,j)[0] not in and detecte_coordonnees_combinaison(grille,i,j) != []
+                ttes_combis += (detecte_coordonnees_combinaison(grille,i,j))
     return ttes_combis
 
-def test_detecte_coordonnees_combinaison():
-    """
-    Test la fonction detecte_coordonnees_combinaison(grille, i, j).
-    Pour chaque cas de test, affiche True si le test passe, False sinon
-    """
-    #fonction détecte coordonnées combinaison commence par coordonnées du point, 
-    #puis du regarde du milieu vers le haut, puis repart du milieu et va vers le 
-    #bas, puis vers la gauche, puis vers la droite
-    
-    # Test1: bonbon situé sur la colonne la plus à gauche, sur la 2e ligne
-    grille = [[1,2,3,4],[1,1,1,1],[1,3,4,4],[2,2,3,1]]
-    i = 1
-    j = 0
-    print(detecte_coordonnees_combinaison(grille,i,j) == [[i,j],[0,0],[2,0],[1,1],[1,2],[1,3]])
-          
-    # Test2 : bonbon situé à aucune ligne ou colonne extrême et qui fait combinaison dans tous les sens    
-    grille=[[1,2,3,4],[2,2,2,2],[1,2,3,4],[1,2,3,4]]
-    i = 1
-    j = 1
-    print(detecte_coordonnees_combinaison(grille,i,j) == [[i,j],[0,1],[2,1],[3,1],[1,0],[1,2],[1,3]])
 
-    
-def gravite(grille):
+def gravité(grille):
     """
     Renvoie une liste a laquelle elle a appliquée la gravité sur toutes les cases
     
@@ -262,7 +236,6 @@ def gen_grille_init(size):
                     grille[i][j]= random.randint(1,4)
         liste_combi = recherche_combinaison_grille(grille)
     return grille
-
 
 def verification_deadlock(grille):
     """Cette fonction vérifie qu'il existe encore une possibilité de créer une combinaison en échangeant deux bonbons
